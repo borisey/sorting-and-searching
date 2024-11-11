b5328520e776f71eb54804492d05f8d377e68b30
borisey <slamsnv@gmail.com>
2024-11-11 12:30:21 +0300

    Init

---
+++ src/main/java/org/sorting_and_searching/Arrays.java
@@ -0,0 +1,86 @@
+package org.sorting_and_searching;
+
+import java.util.Comparator;
+
+public class Arrays {
+
+    // 1
+    static int binarySearch(byte[] a, byte key) {
+
+    }
+
+    // 2
+    static int binarySearch(byte[] a, int fromIndex, int toIndex, byte key) {
+
+    }
+
+    // 3
+    static int binarySearch(char[] a, char key) {
+
+    }
+
+    // 4
+    static int binarySearch(char[] a, int fromIndex, int toIndex, char key) {
+
+    }
+
+    // 5
+    static int binarySearch(double[] a, double key) {
+
+    }
+
+    // 6
+    static int binarySearch(double[] a, int fromIndex, int toIndex, double key) {
+
+    }
+
+    // 7
+    static int binarySearch(float[] a, float key) {
+
+    }
+
+    // 8
+    static int binarySearch(float[] a, int fromIndex, int toIndex, float key) {
+
+    }
+
+    // 9
+    static int binarySearch(int[] a, int key) {
+
+    }
+
+    // 10
+    static int binarySearch(int[] a, int fromIndex, int toIndex, int key) {
+
+    }
+
+    // 11
+    static int binarySearch(long[] a, long key) {
+
+    }
+
+    // 12
+    static int binarySearch(long[] a, int fromIndex, int toIndex, long key) {
+
+    }
+
+    // 13
+    static int binarySearch(short[] a, short key) {
+
+    }
+
+    // 14
+    static int binarySearch(short[] a, int fromIndex, int toIndex, short key) {
+
+    }
+
+    // 15
+    static int binarySearch(T[] a, T key, Comparator c) {
+
+    }
+
+    // 16
+    static int binarySearch(T[] a, int fromIndex, int toIndex, T key, Comparator c) {
+
+    }
+}
---
+++ src/main/java/org/sorting_and_searching/Collections.java
@@ -0,0 +1,15 @@
+package org.sorting_and_searching;
+
+public class Collections {
+
+    // 1
+    static int binarySearch(List> list, T key) {
+
+    }
+
+    // 2
+    static int binarySearch(List list, T key, Comparator c) {
+
+    }
+
+}
